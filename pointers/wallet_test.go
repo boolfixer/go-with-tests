a6b849706c1f4d647d37adaffe6df474b0e3b316
boolfixer <pavel.lysiankou.96@gmail.com>
2024-03-23 17:45:19 +0100

    more experiment with errors and nil

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -3,6 +3,18 @@ package pointers
 import "testing"
 
 func TestWallet(t *testing.T) {
+	assertError := func(t testing.TB, got error, want string) {
+		t.Helper()
+
+		if got == nil {
+			t.Error("wanted an error but didn't get one")
+		}
+
+		if got.Error() != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
@@ -16,13 +28,11 @@ func TestWallet(t *testing.T) {
 		}
 	})
 
-	t.Run("withdraw", func(t *testing.T) {
-		wallet := Wallet{balance: Bitcoin(20)}
-
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
 		err := wallet.Withdraw(Bitcoin(100))
 
-		if err == nil {
-			t.Errorf("wanted an error but didn't get one")
-		}
+		assertError(t, err, "cannot withdraw, insufficient funds")
 	})
 }
